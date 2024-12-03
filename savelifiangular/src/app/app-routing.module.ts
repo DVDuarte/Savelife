2e46a1e1ac5a3a5c440c479c72ae76a11a7fc4a8
Daniel Duarte <danielvitasso@gmail.com>
2024-12-03 16:48:40 -0300

    Cadastro Despesas!

--- savelifiangular/src/app/app-routing.module.ts
+++ savelifiangular/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroDespesasComponent } from './cadastro-despesas/cadastro-despesas.component';
+import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
+  { path: '', component: AppComponent }, 
+  { path: 'cadastro-despesas', component: CadastroDespesasComponent },
 ];
 
 
